0679492aa582714ef75e5536b30e0eaf683f5a3d
Tadatoshi Takahashi <tadatoshi@tadatoshi-takahashis-computer.local>
2010-05-02 16:59:29 -0400

    1. Changed the default host to biocharquebec.org 2. Added long-textfield CSS class.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   def default_url_options(options={})  
     logger.debug "default_url_options is passed options: #{options.inspect}\n"  
     options.merge!({ :locale => I18n.locale })
-    options.merge!({ :host => "tadatoshi.ca" }) if Rails.env.production? 
+    options.merge!({ :host => "biocharquebec.org" }) if Rails.env.production? 
     options
   end
 
