24f080a0c705e21ae3a77ee7e53e2eaee69ceb3d
Tadatoshi Takahashi <tadatoshi@tadatoshi-takahashis-computer.local>
2010-07-30 20:22:25 -0400

    Added validation specs for Admin and User.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,6 +4,7 @@ class Admin < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
   
+  # TODO: Specifying :validatable to devise call above doesn't work like User model. So validation is added here:
   validates :email, :password, :password_confirmation, :presence => true
   validates :password, :confirmation => true
 end
