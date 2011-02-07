367405f7382ea71277585dc44b8b7dc331e22089
Tadatoshi Takahashi <tadatoshi@Tadatoshi-Takahashis-MacBook.local>
2011-02-07 13:39:31 -0500

    In deploy.rb, specified different Thin configuration files for each environment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,16 +6,18 @@ task :staging do
   set_default_environment
   set :application, "biocharquebec"
   set :deploy_to, "~/public_html/#{application}"
+  set :thin_config, "thin_staging.yml"
 end
 
 task :production do
   set :rails_env, "production"
   set :location, "biocharquebec.org"
   set_role
-  set :ruby_directory, "ruby-1.9.2-p136"
+  set :ruby_directory, "ruby-1.9.2-p0"
   set_default_environment
   set :application, "biocharquebec_production"
   set :deploy_to, "~/public_html/#{application}"
+  set :thin_config, "thin_production.yml"  
 end
 
 task :set_default_environment do
@@ -47,39 +49,7 @@ set :user, "ubuntu"
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-namespace :deploy do
-  task :copy_database_configuration do
-    db_config = "#{deploy_to}/shared/db/#{rails_env}.database.yml"
-    run "cp #{db_config} #{release_path}/config/database.yml"
-    
-    mongoid_config = "#{deploy_to}/shared/db/#{rails_env}.mongoid.yml"
-    run "cp #{mongoid_config} #{release_path}/config/mongoid.yml"
-  end
-  
-  after "deploy:update_code", "deploy:copy_database_configuration"
-
-  task :symlink_uploads_directory do
-    uploads_directory = "#{deploy_to}/shared/uploads"
-    run "ln -s #{uploads_directory} #{release_path}/public/uploads"
-  end
-
-  after "deploy:update_code", "deploy:symlink_uploads_directory"
-
-  task :bundle_install do
-    run "rvmsudo bundle install --gemfile #{release_path}/Gemfile --without development test cucumber"
-  end
-
-  after "deploy:update_code", "deploy:bundle_install"  
-  
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-end
-
 # namespace :deploy do
-# 
 #   task :copy_database_configuration do
 #     db_config = "#{deploy_to}/shared/db/#{rails_env}.database.yml"
 #     run "cp #{db_config} #{release_path}/config/database.yml"
@@ -101,21 +71,53 @@ end
 #     run "rvmsudo bundle install --gemfile #{release_path}/Gemfile --without development test cucumber"
 #   end
 # 
-#   after "deploy:update_code", "deploy:bundle_install"
-# 
-#   %w(start stop restart).each do |action| 
-#     desc "#{action} the Thin processes"  
-#     task action.to_sym do
-#       find_and_execute_task("thin:#{action}")
-#     end
+#   after "deploy:update_code", "deploy:bundle_install"  
+#   
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
-# 
-# namespace :thin do  
-#   %w(start stop restart).each do |action| 
-#   desc "#{action} the app's Thin Cluster"  
-#     task action.to_sym, :roles => :app do  
-#       run "thin #{action} -c #{current_path} -e #{rails_env} -C #{current_path}/config/thin.yml"
-#     end
-#   end
-# end
+
+namespace :deploy do
+
+  task :copy_database_configuration do
+    db_config = "#{deploy_to}/shared/db/#{rails_env}.database.yml"
+    run "cp #{db_config} #{release_path}/config/database.yml"
+    
+    mongoid_config = "#{deploy_to}/shared/db/#{rails_env}.mongoid.yml"
+    run "cp #{mongoid_config} #{release_path}/config/mongoid.yml"
+  end
+  
+  after "deploy:update_code", "deploy:copy_database_configuration"
+
+  task :symlink_uploads_directory do
+    uploads_directory = "#{deploy_to}/shared/uploads"
+    run "ln -s #{uploads_directory} #{release_path}/public/uploads"
+  end
+
+  after "deploy:update_code", "deploy:symlink_uploads_directory"
+
+  task :bundle_install do
+    run "rvmsudo bundle install --gemfile #{release_path}/Gemfile --without development test cucumber"
+  end
+
+  after "deploy:update_code", "deploy:bundle_install"
+
+  %w(start stop restart).each do |action| 
+    desc "#{action} the Thin processes"  
+    task action.to_sym do
+      find_and_execute_task("thin:#{action}")
+    end
+  end
+end
+
+namespace :thin do  
+  %w(start stop restart).each do |action| 
+  desc "#{action} the app's Thin Cluster"  
+    task action.to_sym, :roles => :app do  
+      run "thin #{action} -c #{current_path} -e #{rails_env} -C #{current_path}/config/#{thin_config}"
+    end
+  end
+end
